4b8296c8ccd2842439b5cf10a2693fdea686c66d
Diego Pasquali <imdiegopasquali@gmail.com>
2019-06-22 13:23:38 +0200

    Created initial emacs plugin

---
+++ codcut-emacs.el
@@ -0,0 +1,54 @@
+(defgroup codcut nil
+  "Codcut settings."
+  :group 'convenience)
+
+(defcustom codcut-token
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6MiwiYXV0aGVudGljYXRpb25faWQiOjQsImFwcGxpY2F0aW9uX3R5cGUiOjEsImV4cCI6MTU5MjczMzU4OX0.Nxvm99UcYxcXJsIN-ECHrFD7HZK-3TMC8f8kxc3zHC_9ejMbOQk6TEh2mMN_iSiRJ_DWygHJUNQQTu5sizKDZcustA"
+  "Codcut access token."
+  :type '(string)
+  :group 'codcut)
+
+(defconst post-endpoint
+  "http://localhost:8080/api/posts")
+
+(defun get-selected-text ()
+  "Retrieve the current selected text or nil."
+  (if (use-region-p)
+      (buffer-substring (region-beginning) (region-end)) nil))
+
+(defun get-file-extension ()
+  "Retrieve the file extension or nil."
+  (if (buffer-file-name)
+      (file-name-extension (buffer-file-name)) nil))
+
+(defun get-major-mode ()
+  "Retrieve the current major mode."
+  (symbol-name major-mode))
+
+(defun handle-response (status)
+  "Handle Codcut response."
+  (kill-buffer (current-buffer)))
+
+(defun post-code (code description language)
+  "Send a new post to Codcut."
+  (if (not codcut-token)
+      (throw 'request-error (error "You must set codcut-token first.")))
+  (let ((url-request-method "POST")
+        (url-request-extra-headers
+         `(("Authorization" . ,(format "Bearer %s" codcut-token))
+           ("Content-Type" . "application/json")))
+        (url-request-data
+         (json-encode-alist
+          `(("code" . ,code)
+            ("body" . ,description)
+            ("language" . ,language)))))
+    (url-retrieve post-endpoint 'handle-response)))
+
+(defun share-to-codcut ()
+  "Entry point function."
+  (interactive)
+  (let ((code 'get-selected-text)
+        (description (read-string "Enter a description (optional): "))
+        (language 'get-file-extension))
+    (catch 'request-error
+      (post-code code description language))))
